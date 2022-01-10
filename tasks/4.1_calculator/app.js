25efe4cc183c988a35eb1a460d6dbc5a8fafa143
Ernestobolkin <ernest1047@gmail.com>
2022-01-10 11:32:22 +0200

    🐛

--- tasks/4.1_calculator/app.js
+++ tasks/4.1_calculator/app.js
@@ -1,6 +1,7 @@
 const yargs = require("yargs");
 const chalk = require("chalk");
 
+// using the yargs library
 yargs.command({
   command: "add",
   discribe: "adding 2 args",
@@ -37,4 +38,23 @@ yargs.command({
   },
 });
 
-yargs.parse();
+// yargs.parse();
+
+
+// using process 
+if (process.argv[2] === "add") {
+  let sum = +process.argv[3] + +process.argv[4];
+  console.log(sum);
+}
+if (process.argv[2] === "sub") {
+  let sum = +process.argv[3] - +process.argv[4];
+  console.log(sum);
+}
+if (process.argv[2] === "mult") {
+  let sum = +process.argv[3] * +process.argv[4];
+  console.log(sum);
+}
+if (process.argv[2] === "pow") {
+  let sum = +process.argv[3] / +process.argv[4];
+  console.log(sum);
+}
