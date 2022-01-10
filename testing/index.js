aff97d0c4f1c4a8771a0fae20bf716f2ccea7e95
Ernestobolkin <ernest1047@gmail.com>
2022-01-10 09:08:40 +0200

    testing file

---
+++ testing/index.js
@@ -0,0 +1,13 @@
+// const chalk = require("chalk");
+import chalk from 'chalk';
+
+// const notes = require("./utils");
+
+// const msg = notes();
+
+// console.log(msg);
+
+// console.log(chalk.underline.bgRed("test"));
+// console.log(chalk.yellow('Hello world!'));
+// console.log(chalk.green('Hello world!'));
+console.log(chalk.blue("test"));
---
+++ testing/utils.js
@@ -0,0 +1,5 @@
+const notes = () => {
+  return "Your Notes...";
+};
+
+module.exports = notes;
