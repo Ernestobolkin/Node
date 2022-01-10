3e93d1290a4a093da469b942ead8ef33123f77da
Ernestobolkin <ernest1047@gmail.com>
2022-01-10 11:11:24 +0200

    test

--- testing/index.js
+++ testing/index.js
@@ -1,13 +1,81 @@
-// const chalk = require("chalk");
-import chalk from 'chalk';
+const yargs = require("yargs");
+const chalk = require("chalk");
+const fs = require("fs");
+// console.log(process.argv[2]);
 
-// const notes = require("./utils");
+// creating add command
+yargs.command({
+  command: "add",
+  discribe: "add a new note",
+  builder: {
+    title: {
+      discribe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      discribe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log(chalk.yellow("Title ") + argv.title);
+    console.log(chalk.yellow("Body ") + argv.body);
+  },
+});
 
-// const msg = notes();
+// create remove command
+yargs.command({
+  command: "remove",
+  discribe: "remove a note",
+  handler: function () {
+    console.log(chalk.yellow("Removing the note!"));
+  },
+});
 
-// console.log(msg);
+// create list command
+yargs.command({
+  command: "list",
+  discribe: "list the notes",
+  handler: function () {
+    console.log(chalk.yellow("Listing out all the notes!"));
+  },
+});
 
-// console.log(chalk.underline.bgRed("test"));
-// console.log(chalk.yellow('Hello world!'));
-// console.log(chalk.green('Hello world!'));
-console.log(chalk.blue("test"));
+// create read command
+yargs.command({
+  command: "read",
+  discribe: "read the note",
+  handler: function () {
+    console.log(chalk.yellow("read the note!"));
+  },
+});
+yargs.parse();
+
+// if (command === "init") {
+//   const obj = {
+//     name: "Ernest",
+//     lastName: "Tobolkin",
+//     salary: 1234,
+//   };
+//   fs.writeFileSync("data.json", JSON.stringify(obj));
+// }
+
+// if (command === "addSal") {
+//   const data = fs.readFileSync("data.json").toString();
+
+//   let obj = JSON.parse(data);
+//   obj.salary += +process.argv[3];
+//   fs.writeFileSync("data.json", JSON.stringify(obj));
+// }
+
+// if (command === "show") {
+//   const data = fs.readFileSync("data.json").toString();
+//   console.log(chalk.green(data));
+// }
+
+// if (command === "add") {
+// }
+// if (command === " remove") {
+// }
