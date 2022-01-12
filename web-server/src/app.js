a6dedb553bc17367ffab5f179fc2285bd2c78cd2
Ernestobolkin <ernest1047@gmail.com>
2022-01-12 11:25:29 +0200

    🐛

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,53 +3,30 @@ const require = createRequire(import.meta.url);
 
 import chalk from "chalk";
 import express from "express";
-import path from "path";
-// import { dirname } from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+console.log(__filename);
 console.log(__dirname);
 
-const app = express(dirname);
-app.use(express.static(path.join(dirname,'../public/index.html')))
+const app = express();
+app.use(express.static(path.join(__dirname, "../public/index.html")));
+// app.use(express.static(path.join(__dirname,'../public/index.html')))
 
-app.get("", (req, res) => {
-  res.send("indexHtml");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 app.get("/help", (req, res) => {
   res.send({
-    name:"Ernest"
+    name: "Ernest",
   });
 });
 app.get("/about", (req, res) => {
   res.send("testing express lib");
 });
 
-
 app.listen(3000, () => {
-  console.log(chalk.green("Server is up on port "),chalk.yellow("3000"));
-});
-import chalk from "chalk";
-import express from "express";
-import path from "path";
-// import { dirname } from "path";
-
-console.log(__dirname);
-
-const app = express(dirname);
-app.use(express.static(path.join(dirname,'../public/index.html')))
-
-app.get("", (req, res) => {
-  res.send("indexHtml");
-});
-app.get("/help", (req, res) => {
-  res.send({
-    name:"Ernest"
-  });
-});
-app.get("/about", (req, res) => {
-  res.send("testing express lib");
-});
-
-
-app.listen(3000, () => {
-  console.log(chalk.green("Server is up on port "),chalk.yellow("3000"));
+  console.log(chalk.green("Server is up on port "), chalk.yellow("3000"));
 });
