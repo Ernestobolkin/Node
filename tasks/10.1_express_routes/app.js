fc0398abcd941a6e0bb1727c3a8aafb47ffa8bab
Ernestobolkin <ernest1047@gmail.com>
2022-01-12 13:49:50 +0200

    🐛

--- tasks/10.1_express_routes/app.js
+++ tasks/10.1_express_routes/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import chalk from "chalk";
 
-
 const app = express();
 const port = 8080;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Hello    World");
 });
 
 app.post("/users", (req, res) => {
@@ -20,6 +19,43 @@ app.post("/users", (req, res) => {
     },
   };
   res.send(sendBody);
+// post by user:
+//   "data": [
+//     {
+//         "name": "Ernest",
+//         "id": "id1"
+//     },
+//     {
+//         "name": "Daniel",
+//         "id": "id2"
+//     },
+//     {
+//         "name": "Shaked",
+//         "id": "id3"
+//     }
+// ]
+
+// response by server:
+// {
+//   "response": "success using post",
+//   "routeName": "POST",
+//   "body": {
+//       "data": [
+//           {
+//               "name": "Ernest",
+//               "id": "id1"
+//           },
+//           {
+//               "name": "Daniel",
+//               "id": "id2"
+//           },
+//           {
+//               "name": "Shaked",
+//               "id": "id3"
+//           }
+//       ]
+//   }
+// }
 });
 
 app.put("/put", (req, res) => {
@@ -33,6 +69,18 @@ app.put("/put", (req, res) => {
     },
   };
   res.send(sendBody);
+// put by user :
+// {"name":"Shaked","id":"id3"}
+
+// response by server:
+// {
+//   "response": "success using put",
+//   "routeName": "put",
+//   "body": {
+//       "name": "Shaked",
+//       "id": "id3"
+//   }
+// }
 });
 
 app.delete("/delete", (req, res) => {
@@ -46,6 +94,19 @@ app.delete("/delete", (req, res) => {
     },
   };
   res.send(sendBody);
+
+// delete by user:
+// {"name":"Shaked","id":"id3"}
+
+// response by server:
+// {
+//   "response": "success using delete",
+//   "routeName": "delete",
+//   "body": {
+//       "name": "Shaked",
+//       "id": "id3"
+//   }
+// }
 });
 
 app.listen(port, () => {
