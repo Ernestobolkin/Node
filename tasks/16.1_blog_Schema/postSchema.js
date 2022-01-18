c34fa95c94eb74941a6bf5489f58ec04e7e13894
Ernestobolkin <ernest1047@gmail.com>
2022-01-18 13:41:28 +0200

    🐛

--- tasks/16.1_blog_Schema/postSchema.js
+++ tasks/16.1_blog_Schema/postSchema.js
@@ -1,11 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const commentSchema = new Schema({
-  userId: String,
-  comment: String,
-
-})
+const commentSchema = new Schema({ userId: String, content: String });
 
 const postSchema = new Schema({
-  
-})
+  post: String,
+  comments: [commentSchema],
+  likes: Number,
+});
+
+const detailsSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+});
+
+const userSchema = new Schema({
+  details: detailsSchema,
+  posts: postSchema,
+});
+
+
+const User = model("users", userSchema);
+
+module.exports = User;
