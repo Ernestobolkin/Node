0d924afc5e85c97457874a89ebdd048c806d1ccf
Ernestobolkin <ernest1047@gmail.com>
2022-01-18 18:24:35 +0200

    almost done

---
+++ tasks/17.1_product_model/app.js
@@ -0,0 +1,60 @@
+const { connect } = require("mongoose");
+const chalk = require("chalk");
+const Product = require("./productSchema");
+
+connect(
+  "mongodb://localhost/e-commerce",
+  () => {
+    console.log(chalk.yellow("mongoDB connected"));
+  },
+  (e) => console.error(e)
+);
+
+const product1 = {
+  name: "vans pro",
+  category: "shoes",
+  isActive: true,
+  details: {
+    description: "snameproducts nameproducts nameproducts nameproducts",
+    price: 120,
+    images: ["img1", "img2", "img3"],
+    phone: 052,
+  },
+};
+const product2 = {
+  name: "T-Shirt",
+  category: "Shirt",
+  isActive: true,
+  details: {
+    description: "snameproducts nameproducts nameproducts nameproducts",
+    price: 100,
+    discount: 20,
+    images: ["img1", "img2", "img3"],
+    phone: 052,
+  },
+};
+const product3 = {
+  name: "test pro",
+  category: "shoes",
+  isActive: false,
+  details: {
+    description: "snameproducts nameproducts nameproducts nameproducts",
+    price: 120,
+    images: ["img1", "img2", "img3"],
+    phone: 052,
+  },
+};
+
+const addP = async (product1) => {
+  try {
+    const createP = await Product.create(product1);
+    console.log(createP);
+    console.log("The Product Waw Created");
+  } catch (e) {
+    console.log(chalk.red(e));
+  }
+};
+
+addP(product1);
+addP(product2);
+addP(product3);
