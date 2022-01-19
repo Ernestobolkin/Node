0dd57eef8d9774074d50f7436bcd78c71b14ba8e
Ernestobolkin <ernest1047@gmail.com>
2022-01-19 13:01:21 +0200

    🐛

--- tasks/17.1_product_model/app.js
+++ tasks/17.1_product_model/app.js
@@ -1,6 +1,7 @@
 const { connect } = require("mongoose");
 const chalk = require("chalk");
 const Product = require("./productSchema");
+const productArr = require("./productsArr")
 
 connect(
   "mongodb://localhost/e-commerce",
@@ -10,53 +11,15 @@ connect(
   (e) => console.error(e)
 );
 
-const product1 = {
-  name: "id113",
-  category: "shoes",
-  isActive: true,
-  details: {
-    description: "snameproducts nameproducts nameproducts nameproducts",
-    price: 120,
-    images: ["img1", "img2", "img3"],
-    phone: "+972503888569",
-  },
-};
-//
-
-const product2 = {
-  name: "T-Shirt",
-  category: "Shirt",
-  isActive: true,
-  details: {
-    description: "snameproducts nameproducts nameproducts nameproducts",
-    price: 100,
-    discount: 20,
-    images: ["img1", "img2", "img3"],
-    phone: +972123456789111,
-  },
-};
-const product3 = {
-  name: "test pro",
-  category: "shoes",
-  isActive: false,
-  details: {
-    description: "snameproducts nameproducts nameproducts nameproducts",
-    price: 120,
-    images: ["img1", "img2", "img3"],
-    phone: +972542085662,
-  },
-};
-
-const addP = async (product) => {
+const addP = async () => {
   try {
-    const createP = await Product.create(product);
+    const createP = await Product.create(productArr);
     console.log(createP);
-    console.log("The Product Waw Created");
+    console.log("The Products Was Created");
   } catch (e) {
     console.log(chalk.red(e));
   }
 };
 
-addP(product1);
-// addP(product2);
-// addP(product3);
+
+
---
+++ tasks/17.1_product_model/productsArr.js
@@ -0,0 +1,38 @@
+const productsData = [
+  {
+    name: "T-Shirt",
+    category: "Shirt",
+    isActive: true,
+    details: {
+      description: "snameproducts nameproducts nameproducts nameproducts",
+      price: 100,
+      discount: 20,
+      images: ["img1", "img2", "img3"],
+      phone: "+972542085662",
+    },
+  },
+  {
+    name: "test pro",
+    category: "shoes",
+    isActive: false,
+    details: {
+      description: "snameproducts nameproducts nameproducts nameproducts",
+      price: 120,
+      images: ["img1", "img2", "img3"],
+      phone: "+972542085662",
+    },
+  },
+  {
+    name: "id113",
+    category: "shoes",
+    isActive: true,
+    details: {
+      description: "snameproducts nameproducts nameproducts nameproducts",
+      price: 120,
+      images: ["img1", "img2", "img3"],
+      phone: "+972503888569",
+    },
+  },
+];
+
+module.exports = productsData;
