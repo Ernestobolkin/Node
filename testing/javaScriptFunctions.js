e6328dc33609314f5b1f2060b4155d33bbbfd5dd
Ernestobolkin <ernest1047@gmail.com>
2022-01-19 13:02:51 +0200

    Testing

--- testing/javaScriptFunctions.js
+++ testing/javaScriptFunctions.js
@@ -1,20 +1,21 @@
 const validator = require("validator");
 
-const validate = (number) => {
-  // console.log(validator.isMobilePhone("he-IL", number));
-  // console.log(validator.isMobilePhone(number,"he-IL"));
-  // validator.isMobilePhone(unique_key, 'any')
-};
+// const validate = (number) => {
+// console.log(validator.isMobilePhone("he-IL", number));
+// console.log(validator.isMobilePhone(number,"he-IL"));
+// validator.isMobilePhone(unique_key, 'any')
+// };
 
 // validate(+972542086882);
 // validate("+982542086882");
 // validate("0542086882");
 
-const arrValidate = (arg) => {
-  console.log(typeof arg);
-  console.log(arg.length);
-};
+// const arrValidate = (arg) => {
+//   console.log(typeof arg);
+//   console.log(arg.length);
+// };
 
-const tempArr = [];
+// const tempArr = [];
 
-arrValidate(arrValidate);
+// arrValidate(arrValidate);
+
